a09b0911ecde3a7a15297571d4b39e286bc4ad4b
czhou578 <colizu2020@gmail.com>
2024-08-24 17:56:10 -0700

    added all image labels to db

--- nn-model/classify_images.py
+++ nn-model/classify_images.py
@@ -8,12 +8,13 @@ import torch
 import os
 import glob
 import json
+import sqlite3
+
 
 windows_image_paths = "C:\\Users\\mycol\\Documents\\iphone-photos"
 download_url("https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json", ".", "imagenet_class_index.json")
 
 wsl_path = subprocess.check_output(['wslpath', windows_image_paths]).decode().strip()
-# print(f"WSL Path: {wsl_path}")
 
 image_extensions = ["*.HEIC", "*.heic"]
 img_paths = []
@@ -25,9 +26,19 @@ with open("imagenet_class_index.json") as f:
 
 # Invert the dictionary to map indices to class names
 idx_to_class = {int(key): value[1] for key, value in class_idx.items()}
-print(idx_to_class)
 
 register_heif_opener()
+
+def store_predictions(predictions):
+    conn = sqlite3.connect('../database/pictures.db')
+    cursor = conn.cursor()
+
+    cursor.execute("BEGIN TRANSACTION;")
+    for prediction in predictions:
+        cursor.execute("INSERT INTO images (keywords) VALUES (?)", (idx_to_class[prediction.item()], ))
+    conn.commit()
+    conn.close()
+
 
 # Example transform
 transform = transforms.Compose([
@@ -55,14 +66,13 @@ class ImageDataset(Dataset):
 dataset = ImageDataset(img_paths, transform=transform)
 dataloader = DataLoader(dataset, batch_size=32, shuffle=False, num_workers=4)
 
-model = models.resnet101(pretrained=True)
+model = models.resnet101(weights='DEFAULT')
 model.eval()  # Set the model to evaluation mode
 
 with torch.no_grad():  # Disable gradient computation
     for batch in dataloader:
         outputs = model(batch)  # Perform inference
         _, preds = torch.max(outputs, 1)  # Get the predicted class
-        for pred in preds:
-            label = idx_to_class[pred.item()]
-            print(label)
+        print(preds)
+        store_predictions(preds)
 
